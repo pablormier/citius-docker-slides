5af0897217987d4dd893af30d979016002656032
Pablo Rodríguez Mier <pablo.rodriguez.mier@usc.es>
2014-10-05 10:12:14 +0200

    Initial version of the CITIUS HTML5 Slide Template

--- slide_config.js
+++ slide_config.js
@@ -4,15 +4,14 @@ var SLIDE_CONFIG = {
     title: 'Title Goes Here<br>Up To Two Lines',
     subtitle: 'Subtitle Goes Here',
     //eventInfo: {
-    //  title: 'Google I/O',
+    //  title: 'Doctoral Meeting',
     //  date: '6/x/2013'
     //},
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/citius-logo-24x24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -23,14 +22,14 @@ var SLIDE_CONFIG = {
   // Author information
   presenters: [{
     name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
+    company: 'Job Title<br>CiTIUS',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
     github: 'http://github.com/you'
   }/*, {
     name: 'Second Name',
-    company: 'Job Title, Google',
+    company: 'Job Title, CiTIUS',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
     www: 'http://www.you.com',
